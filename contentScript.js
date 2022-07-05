589e2b7261b62063976132f58ae4304e9838d2a9
Terrorknubbel <47216962+Terrorknubbel@users.noreply.github.com>
2022-07-05 13:55:27 +0200

    serien support hinzugefügt

--- contentScript.js
+++ contentScript.js
@@ -1,51 +1,63 @@
-(() => {
-    let header, youtubePlayer;
-    let currentVideo = "";
+let div = document.createElement("div");
+div.className = "download-container";
 
-    chrome.runtime.onMessage.addListener((obj, sender, response) => {
-        const { type, value, videoId } = obj;
+let downloadBtn = document.createElement("button");
+downloadBtn.className = "download-btn";
+downloadBtn.innerText = "Download";
+downloadBtn.addEventListener("click", download);
 
-        if (type === "NEW"){
-            currentVideo = videoId;
-            newVideoLoaded();
-        }
-    })
+div.appendChild(downloadBtn);
 
-    const newVideoLoaded = () => {
-        const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
-        
-        if(!bookmarkBtnExists){
-            const downloadBtn = document.createElement("button");
-            downloadBtn.className = "download-btn";
-            downloadBtn.innerText = "Download amk";
+let actionDiv = document.getElementsByClassName("actions")[0];
+actionDiv.innerHTML = "";   //clear actionDiv
+actionDiv.append(div);
 
-            header = document.getElementsByClassName("actions")[0];
+function download() {
+    let videoTag = document.querySelector("video");
+    let videoSrc = videoTag.src;
+    let videoTitle;
+    let videoType;
 
-            header.append(downloadBtn);
-            downloadBtn.addEventListener("click", downloadImage);
-        }
+    let datailDivChildren = document.getElementsByClassName("detail")[0].children.length;
+    if(datailDivChildren === 6){    //Serie
+        titleArr = document.getElementsByClassName("breadcrumb-item")[2].childNodes[1].title.split("-");
+
+        title = titleArr[0];
+        season = titleArr[1].match(/\d+/)[0];
+        episode = document.getElementsByClassName("breadcrumb-item")[3].childNodes[1].innerText.match(/\d+/)[0];
+
+        videoTitle = title + "- s" + ('0' + season).slice(-2) + "e" + ('0' + episode).slice(-2);
+        videoType = "serie";
+    }else{ // Film
+        videoTitle = document.getElementsByClassName("breadcrumb-item")[2].childNodes[1].title;
+        videoType = "film";
     }
 
-    function downloadImage() {
-        let videoTag = document.querySelector("video");
-        let videoSrc = videoTag.src;
-        let videoTitle = document.getElementsByClassName("breadcrumb-item")[2].childNodes[1].title;
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://192.168.2.108:3000/download");
 
-        let xhr = new XMLHttpRequest();
-        xhr.open("POST", "https://192.168.2.108:3000/download");
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.setRequestHeader("Content-Type", "application/json");
 
-        xhr.setRequestHeader("Accept", "application/json");
-        xhr.setRequestHeader("Content-Type", "application/json");
+    let message = document.createElement("div");
+    message.className = "message";
 
-        xhr.onload = () => alert(xhr.responseText);
+    xhr.onload = () => {
+        message.innerText = "Successfully added to queue.";
+        actionDiv.append(message);
+    };
 
-        let data = `{
-        "videoSrc": "${videoSrc}",
-        "videoTitle": "${videoTitle}"
-        }`;
+    xhr.onerror = () => {
+        message.innerText = "Request failed.";
+        message.classList.add("error");
+        actionDiv.append(message);
+    };
 
-        xhr.send(data);
-    }
+    let data = {
+        "videoType": videoType,
+        "src": videoSrc,
+        "title": videoTitle
+    };
 
-    newVideoLoaded();
-})();
+    xhr.send(data);
+}
